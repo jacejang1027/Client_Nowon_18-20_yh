7f221531f9e2b9260214b1bde10976450814f89e
youngheun3 <101630757+youngheun3@users.noreply.github.com>
2022-03-23 19:52:26 +0900

    C
    ControlStatement_If_Loop

---
+++ Client_C/ControlStatement/While.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+//While : 특정 조건에 따라 내용을 반복하는 반복 제어
+// 형태 : while (조건) {조건이 참일때 반복할 내용 }
+
+//do while : 내용을 일단 한번 실행하고 특정 조건에 따라 내용을 반복제어
+//형태 : do { 조건이 참일때 반복할 내용 ) while (조건)
+//continue 해당 조건이 끝날때까지 계속반복 (밑에 조건문은 생략하고)
+
+int main(void) {
+
+	// 조건이 항상 참인 반복문을 무한 루프라고 함.
+	while (0)
+	{
+		printf("hahahaha11111");
+		continue; // 분기문, 가장 가까운 반복문을 찾아서 해당 반복문의 조건 비교줄로 돌아감
+		printf("hahahaha22222");
+
+	}
+	printf("while is statement is over\n");
+
+
+	int count = 0;
+	while (count <= 5)
+	{
+		printf("while count : %d\n", count);
+		count++;
+
+	}
+	printf("while statement is over\n");
+
+
+	//do while
+	do
+	{
+		printf("do while test");
+	} while (0);
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
